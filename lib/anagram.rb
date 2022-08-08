d8fce728c4c2da85f7dc464593cb409ab4cb3942
Rabah <rmerzoug471@gmail.com>
2022-08-07 21:58:08 -0500

    commit tree 8d50e2cd0972b6924fd8fb73cb364fe5e4d77c61

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram 
+    def initialize(word)
+        @word = word
+    end
+    def match(array = "")
+        # matches = array.select do |arr|
+        #     arr == @word
+        end
+        
+        
+    end
+end 
